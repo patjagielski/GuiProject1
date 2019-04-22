7e1a4d762f9e7e7bc5892c5137c1e6cc181175dd
patjagielski <jagielskipat@gmail.com>
2019-04-22 11:33:17 +0200

    Update

--- src/Shapes.java
+++ src/Shapes.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
@@ -14,23 +15,9 @@ public abstract class Shapes extends JComponent{
     int []polyx;
     int[] polyy;
     String objectType;
+    int scalex = getWidth() / Shapes.MAX_WIDTH;
+    int scaley = getHeight() / Shapes.MAX_HEIGHT;
 
-/*
-    public void paint(Graphics g) {
-
-        //set random color
-       // int sideno = rainbow.nextInt(7)+3;
-
-        /*
-        double theta = 2 * Math.PI/sideno;
-        int[] arrx = new int[sideno];
-        int[] arry = new int[sideno];
-        for(int i =0;i<sideno;i++){
-            arrx[i] = getWidth()/2 +(int)(Math.cos(theta * i)*50);
-            arry[i] = getHeight()/2 + (int)(Math.sin(theta * i)*50);
-
-    }
-    */
     public static Color rainbow() {
         Random rainbow = new Random();
         int red = rainbow.nextInt(255);
@@ -39,7 +26,9 @@ public abstract class Shapes extends JComponent{
         return new Color(red, blue, green);
     }
 
-    }
+}
+
+
 
 
 class createShapes implements Runnable {
@@ -60,11 +49,13 @@ class createShapes implements Runnable {
             System.out.println(x + " | " + y);
             shapes.add(new PPolygon(x, y));
             shapes.add(new POval(x, y));
+
+
         }
 
-            double scalex = (double) frame.getWidth() / Shapes.MAX_WIDTH;
-            double scaley = (double) frame.getHeight() / Shapes.MAX_HEIGHT;
-        System.out.printf(scalex+" "+scaley);
+        double scalex = (double) frame.getWidth() / Shapes.MAX_WIDTH;
+        double scaley = (double) frame.getHeight() / Shapes.MAX_HEIGHT;
+        System.out.printf(scalex + " " + scaley);
 /*
             for (Shapes no : shapes) {
                 no.x *= scalex;
@@ -80,16 +71,51 @@ class createShapes implements Runnable {
            }
            */
 
-            frame.repaint();
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        frame.repaint();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
 
-            }
+        }
 
     }
+
+    public void read() throws IOException {
+        try {
+            FileWriter fw = new FileWriter("GUIProjects");
+            Writer output = new BufferedWriter(fw);
+            int sz = shapes.size();
+            for (int i = 0; i < sz; i++) {
+                output.write(shapes.get(i).toString() + " ");
+            }
+            output.close();
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+
+    public void write(){
+        String s;
+        try{
+            BufferedReader input = new BufferedReader(new FileReader("GUIProjects.txt"));
+            if(!input.ready()){
+                throw new IOException();
+            }
+
+            int sz = shapes.size();
+            for(int i =0; i<sz; i++){
+                System.out.println(shapes.get(i).toString());
+            }
+        }catch (IOException e){
+            System.err.println(e);
+        }
+
+    }
+
+
 }
+
 
 
     class POval extends Shapes {
@@ -99,11 +125,42 @@ class createShapes implements Runnable {
         POval(int x, int y) {
             super();
             objectType = "Oval";
-            this.x = x;
-            this.y = y;
-            this.width = (int) (Math.random() * 100);
-            this.rad = (int) (Math.random() * 100);
+            this.x = getWidth()*scalex;
+            this.y = getHeight()*scaley;
+            this.width = (int)(Math.random()*MAX_WIDTH);
+            this.rad = (int)(Math.random()*MAX_HEIGHT/2);
 
+
+        }
+        public String objType(){
+            return objectType;
+        }
+        public Color getRGB(){
+            return POval.rainbow();
+        }
+        public ArrayList getDimensions(){
+            int count = 0;
+            ArrayList<Integer> dimO = new ArrayList<>();
+            for(int i = 0; i<4; i++){
+                if(count == 0) {
+                  dimO.add(x);
+                    count++;
+                }else if(count == 1){
+                    dimO.add(y);
+                    count++;
+                }else if(count == 2){
+                    dimO.add(width);
+                    count++;
+                }
+                else{
+                    dimO.add(rad);
+                }
+            }
+            return dimO;
+        }
+        @Override
+        public String toString(){
+            return  "|" +objType() + " " + getRGB() + " " + getDimensions() + " |";
         }
 
         @Override
@@ -112,59 +169,81 @@ class createShapes implements Runnable {
             g.setColor(g1);
             g.drawOval(x, y, width, rad);
             repaint();
-            while(x > MAX_WIDTH ||  y>MAX_HEIGHT){
-                x= 0;
-                y =0;
-            }
-            while(x<0 || y<0){
-                x = 0;
-                y = 0;
-            }
         }
 
-        @Override
-        public String toString() {
-            return null;
-        }
+
     }
 
-    class PPolygon extends Shapes {
+    class PPolygon extends Shapes{
         Color g1 = Shapes.rainbow();
         Random rand = new Random();
 
         PPolygon(int x, int y) {
             super();
             objectType = "Polygon";
-            nosides = rand.nextInt(3) + 3;
+            nosides = rand.nextInt(7) + 3;
             polyx = new int[nosides];
             polyy = new int[nosides];
             for (int i = 0; i < nosides; i++) {
                 polyx[i] = rand.nextInt(x);
                 polyy[i] = rand.nextInt(y);
-
             }
         }
+        /*
+
+        //set random color
+       // int sideno = rainbow.nextInt(7)+3;
+        double theta = 2 * Math.PI/sideno;
+        int[] arrx = new int[sideno];
+        int[] arry = new int[sideno];
+        for(int i =0;i<sideno;i++){
+            arrx[i] = getWidth()/2 +(int)(Math.cos(theta * i)*50);
+            arry[i] = getHeight()/2 + (int)(Math.sin(theta * i)*50);
+
+    }
+    */
+        public String ObjName(){
+            return objectType;
+        }
+        public Color ShapeRGB(){
+           return PPolygon.rainbow();
+        }
+        public ArrayList<Integer> getDimensions(){
+            int count = 0;
+            int it = 0;
+            ArrayList<Integer> dim = new ArrayList<>();
+            for(int i = 0; i<it; i++){
+                if(count == 0) {
+                    dim.add(polyx[it]);
+                   count++;
+                }else{
+                    dim.add(polyy[it]);
+                    count--;
+                    it++;
+                }
+            }
+            return dim;
+        }
+        @Override
+        public String toString(){
+            return "/" + ObjName() + " " + ShapeRGB() + " " + getDimensions() + "/";
+        }
+
+
 
         @Override
         public void paint(Graphics g) {
             super.paintComponent(g);
             g.setColor(g1);
             g.drawPolygon(polyx, polyy, nosides);
-            // int sideno = rainbow.nextInt(7)+3;
-            while(x > MAX_WIDTH || x< 0 || y>MAX_HEIGHT || y<0){
-                x = 0;
-                y = 0;
-            }
+
+
         }
 
-        @Override
-        public String toString() {
-            return super.toString();
-        }
     }
 
     class Main {
-        public static void main(String[] args) throws InterruptedException {
+        public static void main(String[] args) throws InterruptedException   {
             boolean endprogram = false;
             Scanner scan = new Scanner(System.in);
             System.out.println("START - run the program");
@@ -191,5 +270,7 @@ class createShapes implements Runnable {
             }
 
         }
+
+
     }
 
